5d92a4ec30ac5d48c34b21fcbf3c603e3791b275
gauravjadwani <gaurav.jadwani@hotmail.com>
2017-12-08 12:02:52 +0530

    redraw ui integrated

--- my-app/src/Game.js
+++ my-app/src/Game.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 // import {Button} from 'react-bootstrap';
 import FaIcon from 'react-icons/lib/fa/star';
+import Check from 'react-icons/lib/fa/check';
+import Times from 'react-icons/lib/fa/times-circle';
+import Ref from 'react-icons/lib/fa/refresh';
 import * as lodash from 'lodash';
 
 
@@ -15,11 +18,36 @@ console.log(props);
 );
 };
 const Button=(props)=>{
+  console.log(props,'button props');
+  let button;
+  switch (props.answerIsCorrect) {
+    case true:
+    button=
+    <button type="button" className="btn btn-success" onClick={() => props.acceptAnswer()}>
+    <i className="fa fa-check"><Check/></i>
+    </button>
+      break;
+      case false:
+      button=
+      <button type="button" className="btn btn-danger">
+      <i className="fa fa-times">
+      <Times/></i>
+      </button>
+        break;
+    default:
+    button=
+    <button type="button" className="btn btn-success" onClick={()=> props.checkAnswer()}>
+    =
+    </button>
+
+  }
+  console.log(props);
   return(
   <div className="col-md-2">
-  <button type="button">==</button>
+  {button}
+  <Redraw/>
   </div>
-);
+  );
 }
 const Answer=(props)=>{
   return(
@@ -33,12 +61,21 @@ const Answer=(props)=>{
 );
 }
 
+const Redraw=(props) =>{
+  return(<div>
+    <button className="btn btn-warning">
+    <Ref/>
+    </button>
+    </div>)
+}
+
 const Numbers=(props)=>{
   const check=(number)=>{
+    if(props.usedNumbers.indexOf(number)>=0){
+      return 'used ';
+    }
     if(props.selectedNumbers.indexOf(number)>=0){
-
-      console.log(number);
-            return 'selected';
+      return 'selected';
     }
   }
   return(
@@ -56,27 +93,51 @@ Numbers.list=lodash.range(1,10);
 class Game extends Component{
   state={
     selectedNumbers:[],
-    randomnoofstarts:1+Math.floor(Math.random()*9)
+    randomnoofstarts:1+Math.floor(Math.random()*9),
+    answerIsCorrect:null,
+    usedNumbers:[]
   }
   selectedNumber=(clickedNumber)=>{
       if(this.state.selectedNumbers.indexOf(clickedNumber)>=0){
         return;
       }
     this.setState(prevState=>({
+      answerIsCorrect:null,
       selectedNumbers:prevState.selectedNumbers.concat(clickedNumber)
     }))
 
-};
-unselectedNumber=(clickedNumber)=>{
-    // if(this.state.selectedNumbers.indexOf(clickedNumber)>=0){
-    //   return;
-    // }
+  };
+  unselectedNumber=(clickedNumber)=>{
+
   this.setState(prevState=>({
     selectedNumbers:prevState.selectedNumbers.filter(number=>number !== clickedNumber )
   }))
 
-};
+  };
+  checkAnswer=()=>{
+    console.log('clicked');
+    this.setState(prevState=>({
+      answerIsCorrect:(prevState.randomnoofstarts===prevState.selectedNumbers.reduce((sum,number)=>sum+number))
+    }))
+    console.log(this.state.answerIsCorrect,'query');
+  };
+  acceptAnswer=()=>{
+    // save the state of used numbers
+    console.log('answer accepted');
+    this.setState(prevState=>({
+      usedNumbers:(prevState.usedNumbers.concat(prevState.selectedNumbers)),
+      selectedNumbers:[],
+      answerIsCorrect:null,
+      randomnoofstarts:1+Math.floor(Math.random()*9)
+    }))
+  };
+
   render(){
+    // const {
+    //   selectedNumbers,
+    //   randomnoofstarts,
+    //   answerIsCorrect
+    // }=this.state;
   return (
     <div className="container">
     <h3>  play game </h3>
@@ -84,11 +145,12 @@ unselectedNumber=(clickedNumber)=>{
 
     <div className="row">
     <Stars starsstate={this.state.randomnoofstarts}/>
-    <Button/>
+    <Button selectedNumbers={this.state.selectedNumbers} answerIsCorrect={this.state.answerIsCorrect} checkAnswer={this.checkAnswer} acceptAnswer={this.acceptAnswer}/>
+
     <Answer selectedNumbers={this.state.selectedNumbers} unselectedNumber={this.unselectedNumber}/>
     </div>
     <br/>
-    <Numbers selectedNumbers={this.state.selectedNumbers} selectedNumber={this.selectedNumber}/>
+    <Numbers selectedNumbers={this.state.selectedNumbers} selectedNumber={this.selectedNumber} usedNumbers={this.state.usedNumbers}/>
 
     </div>
   );
